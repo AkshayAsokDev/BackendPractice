294c473da92db3f8e7b7e1922505fefa82d3b538
Akshay Asok <akshayasokson@gmail.com>
2025-12-09 21:44:52 +0530

    readme update, connecting to mongoDB using mongoose module

--- 02_Fitness/index.js
+++ 02_Fitness/index.js
@@ -7,15 +7,26 @@ const express = require("express");
 const server = express();
 require("dotenv").config();
 
+
 const PORT = process.env.PORT;
 const HomeRoute = require("./Routes/HomeRoute");
 const ActivityRoute = require("./Routes/ActivityRoute");
+const { default: mongoose } = require("mongoose");
+
+
 
 // Home route leader
 server.use("/", HomeRoute);
 
 // Api functionalities
 server.use("/api/v1", ActivityRoute);
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const DBNAME = process.env.DBNAME;
+mongoose.connect(MONGODB_URL + DBNAME).then(() => {
+    console.log("Connected to MongoDB");
+})
+
 
 server.listen(PORT, () => {
     console.log("Fitness server has been started at Port ", PORT);
